243191afbcd99a6681f18bf20f84931f2fd8f58e
Benjamin Le Heup <37462665+bleheup@users.noreply.github.com>
2019-10-13 22:00:51 -0400

    Update login.php

--- eventManagerAPI/login.php
+++ eventManagerAPI/login.php
@@ -29,7 +29,7 @@ else
     //$arr = array('userid' => "$userid", 'password' =>  "$pword", 'username' => "$uname");
     //echo json_encode($row);
 
-    rsendResultInfoAsJson(json_encode($row));
+    sendResultInfoAsJson(json_encode($row));
   }
   
   else
