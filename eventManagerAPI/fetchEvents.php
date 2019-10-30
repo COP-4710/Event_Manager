fbc16bb32a80642987d1cdb95b51737d653bff37
Benjamin Le Heup <37462665+bleheup@users.noreply.github.com>
2019-10-29 22:38:19 -0400

    Add files via upload

---
+++ eventManagerAPI/fetchEvents.php
@@ -0,0 +1,43 @@
+<?php
+$conn = new mysqli("localhost", "debian-sys-maint", "d197cf7871616e0bcada971f428d1f69d5164d01474837e7", "event_manager");
+$inData = getRequestInfo();
+if ($conn->connect_error)
+{
+		returnWithError( $conn->connect_error );
+}
+else
+{
+	$date = $inData["date"];
+	$sql = "SELECT * FROM event where date='" . $date  . "'";
+		//$result = $conn->query($sql);
+	$result = mysqli_query($conn, $sql);
+    	$json_array = array();
+	if($result->num_rows > 0 )
+	{
+   		while($row = mysqli_fetch_assoc($result))
+    		{
+      			$json_array[] = $row;
+    		}
+    		sendResultInfoAsJson(json_encode($json_array));
+		$conn->close();
+	}
+	else
+	{
+		$object->err = "There were no events on the"
+		returnWithError($object);
+	}
+}
+function returnWithError( $err )
+{
+		sendResultInfoAsJson(json_encode($err));
+}
+function getRequestInfo()
+{
+		return json_decode(file_get_contents('php://input'), true);
+}
+function sendResultInfoAsJson( $obj )
+{
+		header('Content-type: application/json');
+		echo $obj;
+}
+?>
