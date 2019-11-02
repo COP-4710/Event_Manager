3d2a32c22278a28a52afb2cb37c8018343321b87
bmasondavis <54715795+bmasondavis@users.noreply.github.com>
2019-11-01 23:30:49 -0400

    Add files via upload

---
+++ eventpage.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang ="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<script src="emanager.js"></script>
+	<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
+
+	<title>Knight Events</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<section>
+			<div class="userBox">
+				<div class="content">
+					<h1>This is a test.</h1>
+					<p> </p>
+						<a href="#">logout</a>
+						<a href="#">Create Event</a>
+						<a href="#">Edit account</a>
+					</div>
+				</div>
+				<div class="events">
+					<ul>
+						<li>
+								<div class="time">
+									<h2>24<br><span>June</span><h2>
+								</div>
+									<div class = "details">
+									<h3>event_name</h3>
+									<p>description
+									</p>
+									<sript></script>
+									<a href="#">View Details</a>
+								</div>
+						</li>
+					</ul>
+					</div>
+
+			</section>
+
+
+	</body>
+</html>
